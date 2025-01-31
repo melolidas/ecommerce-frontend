0b6c978cef88b8612a20c41ee09b866ca4770dd5
theHalid <the_halidcastle@mail.ru>
2025-01-31 18:40:10 +0600

    intermidiate featured product done

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
+import Featured from "@/components/Featured";
 import Header from "@/components/Header";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
 
-export default function HomePage() {
+export default function HomePage({ product }) {
+    console.log(product);
     return (
         <div>
             <Header />
+            <Featured product={product} />
         </div>
     );
+}
+
+export async function getServerSideProps() {
+    const featuredProductId = "679bde9132b0ef5b71cec006";
+    await mongooseConnect();
+    const product = await Product.findById(featuredProductId);
+    return {
+        props: { product: JSON.parse(JSON.stringify(product)) },
+    };
 }
