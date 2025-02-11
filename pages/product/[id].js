d29171f446b17ca0ecc5c3b8b18507103f5039e4
theHalid <the_halidcastle@mail.ru>
2025-02-12 05:54:25 +0600

    cart animation done

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -10,6 +10,7 @@ import Button from "@/components/Button";
 import CartIcon from "@/components/icons/CartIcon";
 import { useContext } from "react";
 import { CartContext } from "@/components/CartContext";
+import FlyingButton from "@/components/FlyingButton";
 
 const ColWrapper = styled.div`
     display: grid;
@@ -47,13 +48,14 @@ export default function ProductPage({ product }) {
                                 <Price>${product.price}</Price>
                             </div>
                             <div>
-                                <Button
-                                    primary
-                                    onClick={() => addProduct(product._id)}
+                                <FlyingButton
+                                    main
+                                    _id={product._id}
+                                    src={product.images?.[0]}
                                 >
                                     <CartIcon />
                                     Add to cart
-                                </Button>
+                                </FlyingButton>
                             </div>
                         </PriceRow>
                     </div>
