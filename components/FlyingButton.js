d11fd396b18b6cba6ddfe78ee23c991131eda104
theHalid <the_halidcastle@mail.ru>
2025-02-21 02:56:53 +0600

    bugs fixed

--- components/FlyingButton.js
+++ components/FlyingButton.js
@@ -1,54 +1,49 @@
-import FlyingButtonOriginal from "react-flying-item";
 import { primary } from "@/lib/colors";
 import { ButtonStyle } from "./Button";
 import styled from "styled-components";
 import { useContext } from "react";
 import { CartContext } from "./CartContext";
 
-const FlyingButtonWrapper = styled.div`
-    button {
-        ${ButtonStyle}
-        ${(props) =>
-            props.main
-                ? `
-    background-color: ${primary};
-    color: white;
-    `
-                : `
-    background-color:transparent;
-    border: 1px solid ${primary};
-    color: ${primary};
-    
+const FlyingButtonWrapper = styled.button`
+    ${ButtonStyle}
+    ${(props) =>
+        props.main
+            ? `
+        background-color: ${primary};
+        color: white;
+        `
+            : `
+        background-color:transparent;
+        border: 1px solid ${primary};
+        color: ${primary};
     `}
     ${(props) =>
-            props.white &&
-            `
+        props.white &&
+        `
         background-color:white;
         border: 1px solid white;
+        color: black;
     `}
+    cursor: pointer;
+    transition: 0.2s;
+    border-radius: 8px;
+    display: flex;
+    justify-content: center;
+    &:hover {
+        opacity: 0.8;
     }
 `;
 
-export default function FlyingButton(props) {
+export default function FlyingButton({ white, main, _id, children }) {
     const { addProduct } = useContext(CartContext);
+
     return (
         <FlyingButtonWrapper
-            white={props.white}
-            main={props.main}
-            onClick={() => addProduct(props._id)}
+            white={white}
+            main={main}
+            onClick={() => addProduct(_id)}
         >
-            <FlyingButtonOriginal
-                {...props}
-                targetTop={"5%"}
-                flyingItemStyling={{
-                    width: "auto",
-                    height: "auto",
-                    maxWidth: "60px",
-                    maxHeight: "60px",
-                    borderRadius: 0,
-                }}
-                targetLeft={"80%"}
-            />
+            {children || "Добавить в корзину"}
         </FlyingButtonWrapper>
     );
 }
